9f450be517689f319bb0290a95ae77230ae894db
eloisa <144168277+limaeloisa@users.noreply.github.com>
2025-10-02 13:39:14 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+import mysql.connector
+
+app = Flask(__name__)
+
+def connectar_banco():
+    return mysql.connector.connect(
+        host="localhost"
+        user="seu_usuario", 
+        password="sua_senha",
+        database="Ajudai" 
+    )
+@app.route("/", methods=["GET", "POST"])
+
+def index():
+    if request.method == "POST":
+        nome = request.form["nome"]
+        email = request.form["email"]
+        senha = request.form["senha"]
+        telefone = request.form["telefone"]
+
+        conn = connectar_banco()
+        cursor = conn.cursor()
+        sql = "INSERT INTO usuarios (nome, email, senha, telefone) VALUES (%s, %s, %s, %s)"
+        cursor.execute(sql, (nome, email, senha, telefone))
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+        return "Dados salvos com sucesso!"
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
