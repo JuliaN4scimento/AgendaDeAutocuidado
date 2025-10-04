76205172c449f140e24131532566e5ca36c38565
giovanna153 <o.giovanna@escolar.ifrn.edu.br>
2025-10-04 00:39:20 -0300

    atualizção

--- app.py
+++ app.py
@@ -1,36 +1,28 @@
-from flask import Flask, render_template, request, redirect, url_for
-import mysql.connector
+from flask import Flask 
+from flask import render_template, request, redirect, url_for, session, flash
+import mysql.connector as connection 
 
 app = Flask(__name__)
 
-def connectar_banco():
-    return mysql.connector.connect(
-        host="localhost",
-        user="seu_usuario", 
-        password="sua_senha",
-        database="Ajudai"
+app.secret_key = "segredo123"
+
+@app.route ("/")
+def conexao ():
+    cnx = connection.MySQLConnection (
+        host="127.0.0.1",
+        user="root", 
+        password="labinfo",
+        database="ajudaibd"
     )
 
-@app.route("/", methods=["GET", "POST"])
-def index():
-    if request.method == "POST":
-        nome = request.form["nome"]
-        email = request.form["email"]
-        senha = request.form["senha"]
-        telefone = request.form["telefone"]
+    cursor = cnx.cursor (dictionary = True)
+    cursor.execute ("SELECT nome, telefone, data_nasc, email, senha FROM usuarios")
+    
+    resultado = cursor.fetchall ()
 
-        conn = connectar_banco()
-        cursor = conn.cursor()
-        sql = "INSERT INTO usuarios (nome, email, senha, telefone) VALUES (%s, %s, %s, %s)"
-        cursor.execute(sql, (nome, email, senha, telefone))
-        conn.commit()
-        cursor.close()
-        conn.close()
+    return render_template ("cadastro.html", banco = resultado)
+ 
+@app.route("/cadastro")
+def cadastro():
+    return render_template("cadastro.html")
 
-        return redirect(url_for("tipoagenda"))
-
-    return render_template("index.html")
-
-@app.route("/tipoagenda")
-def tipoagenda():
-    return render_template("tipoagenda.html")
