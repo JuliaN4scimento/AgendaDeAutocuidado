239df56bea2e809a1218e88923fd595fdc3ed915
Julia Nascimento <julia.nascimento1@escolar.ifrn.edu.br>
2025-10-06 15:46:32 -0300

    atualização

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for
+#Importações
+from flask import Flask, render_template, request, redirect, url_for, session
 import mysql.connector
 
 #Aplicação flask
@@ -58,6 +59,25 @@ def agendadiaria():
 def agendasemanal():
     return render_template('agenda_semanal.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        senha = request.form['senha']
+
+        comando = "SELECT * FROM usuario WHERE email = %s AND senha = %s"
+        valores = (email, senha)
+        cursor.execute(comando, valores)
+        usuario = cursor.fetchone()
+
+        if usuario:
+            session['usuario_email'] = email  # Guarda o usuário logado
+            return redirect(url_for('tipoagenda'))
+        else:
+            return render_template('login.html', erro='E-mail ou senha incorretos.')
+
+    return render_template('login.html')
+
 #Rota responsavel por inserção de dados na agenda diaria
 @app.route('/final_diaria', methods=['POST'])
 def final_diaria():
@@ -68,11 +88,10 @@ def final_diaria():
 
     for h, a in zip(horarios, atividades):
         comando = "INSERT INTO agenda_diaria (email_usuario, dia, horario, atividade) VALUES (%s, %s, %s, %s)"
-        cursor.execute(comando, (email, dia, horarios, atividades))
+        cursor.execute(comando, (email, dia, h, a))
     conexao.commit()
 
     return "Agenda salva com sucesso!"
-
 
 #Rodar o flask
 if __name__ == '__main__':
